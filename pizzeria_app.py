1b677ebed3a5cb464a99baf5d2818452759ccc3d
Arsemenko Viktoria <arsemenko.vika@mail.ru>
2026-04-18 17:15:13 +0300

    created basic pizza class model

---
+++ pizzeria_app.py
@@ -0,0 +1,10 @@
+import json
+
+class Pizza:
+   def __init__(self, name, ingredients, price):
+       self.name = name
+       self.ingredients = ingredients
+       self.price = price
+
+   def __str__(self):
+       return f"{self.name} [{', '.join(self.ingredients)}] - {self.price} руб."
