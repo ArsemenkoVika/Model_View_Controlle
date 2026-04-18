24aa17fafe4c07003e58d05b7daa1e7ec8d40ae0
Arsemenko Viktoria <arsemenko.vika@mail.ru>
2026-04-18 18:16:29 +0300

    Finalize app with interactive menu

--- pizzeria_app.py
+++ pizzeria_app.py
@@ -9,7 +9,6 @@ class Pizza:
    def __str__(self):
        return f"{self.name} [{', '.join(self.ingredients)}] - {self.price} руб."
 
-
 class PizzaFactory:
    @staticmethod
    def create_pizza(choice, custom_data=None):
@@ -26,7 +25,6 @@ class PizzaFactory:
            return Pizza(custom_data['name'], custom_data['ingreds'], custom_data['price'])
        return None
 
-
 class StatsManager:
    FILE_NAME = "sales.json"
 
@@ -42,13 +40,11 @@ class StatsManager:
        with open(StatsManager.FILE_NAME, "w", encoding="utf-8") as f:
            json.dump(data, f, ensure_ascii=False, indent=4)
 
-
    @staticmethod
    def show_admin_stats():
        try:
            with open(StatsManager.FILE_NAME, "r") as f:
                orders = json.load(f)
-
        except:
            print("история заказов пуста")
            return
@@ -59,3 +55,44 @@ class StatsManager:
        print(f"Продано пицц: {len(orders)}")
        print(f"Общая выручка: {total_revenue} руб.")
        print(f"Чистая прибыль (30%): {profit} руб.")
+
+class PizzeriaApp:
+   def __init__(self):
+       self.factory = PizzaFactory()
+       self.toppings = {"Сыр": 50, "Грибы": 40, "Бекон": 80}
+
+   def start(self):
+       while True:
+           print("\n1. заказать пиццу\n2. Админ-панель\n3. Выход")
+           cmd = input("Выберите действие: ")
+           if cmd == "1":
+               self.make_order()
+           elif cmd == "2":
+               StatsManager.show_admin_stats()
+           elif cmd == "3":
+               break
+
+   def make_order(self):
+       print("\nМеню: 1-Маргарита, 2-Пепперони, 3-4 Сыра, 4-Гавайская, 5-Мясная, 6-Свой рецепт")
+       choice = input("ваш выбор: ")
+       custom_data = None
+       if choice == "6":
+           name = input("Название вашей пиццы: ")
+           ingreds = input("ингредиенты (через запятую): ").split(",")
+           price = int(input("Ваша цена: "))
+           custom_data = {"name": name, "ingreds": ingreds, "price": price}
+
+       pizza = self.factory.create_pizza(choice, custom_data)
+       if not pizza: return
+
+       final_price = pizza.price
+       if input("Добавить топпинги? (y/n): ") == "y":
+           for top, cost in self.toppings.items():
+               if input(f"Добавить {top} (+{cost} руб.)? (y/n): ") == "y":
+                   final_price += cost
+
+       print(f"\nВаш заказ: {pizza.name}. Итого: {final_price} руб.")
+       StatsManager.save_order(pizza.name, final_price)
+
+if __name__ == "__main__":
+   PizzeriaApp().start()
