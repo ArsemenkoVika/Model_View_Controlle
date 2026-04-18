8b1197460c5c228b73b208ebb6fc4d5556ed2b3d
Arsemenko Viktoria <arsemenko.vika@mail.ru>
2026-04-18 17:41:59 +0300

    Simple Factory pattern for pizza creation

--- pizzeria_app.py
+++ pizzeria_app.py
@@ -8,3 +8,20 @@ class Pizza:
 
    def __str__(self):
        return f"{self.name} [{', '.join(self.ingredients)}] - {self.price} руб."
+
+
+   class PizzaFactory:
+       @staticmethod
+       def create_pizza(choice, custom_data=None):
+           recipes = {
+               "1": ("Маргарита", ["томаты", "моцарелла"], 300),
+               "2": ("Пепперони", ["пепперони", "моцарелла"], 400),
+               "3": ("4 Сыра", ["чеддер", "пармезан", "дорблю", "моцарелла"], 450),
+               "4": ("Гавайская", ["курица", "ананас"], 380),
+               "5": ("Мясная", ["бекон", "говядина", "соус барбекю"], 500)
+           }
+           if choice in recipes:
+               return Pizza(*recipes[choice])
+           elif choice == "6" and custom_data:
+               return Pizza(custom_data['name'], custom_data['ingreds'], custom_data['price'])
+           return None
